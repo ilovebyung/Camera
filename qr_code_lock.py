bec3d4c6996c86394fa1cd63074c497c3a3f0b13
ilovebyung <45321943+ilovebyung@users.noreply.github.com>
2020-12-02 19:30:17 +0900

    full screen window

--- qr_code_lock.py
+++ qr_code_lock.py
@@ -3,51 +3,52 @@ import time
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
-import multiprocessing  
+import multiprocessing
 
 # opencv setup
 font = cv2.FONT_HERSHEY_SIMPLEX
 cap = cv2.VideoCapture(0)
 
+# full screen window
+cv2.namedWindow("frame", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 def unlock(seconds):
-    #GPIO setup
+    # GPIO setup
     pin = 26
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    #unlock 
+    # unlock
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
     time.sleep(seconds)
-    #lock
-    GPIO.output(pin, GPIO.LOW)    
+    # lock
+    GPIO.output(pin, GPIO.LOW)
     GPIO.cleanup()
 
-def read_qr( barcode):
+
+def read_qr(barcode):
     data = barcode.data.decode('utf-8')
-    print(data)  
+    print(data)
     pts = np.array([barcode.polygon], np.int32)
     pts = pts.reshape((-1, 1, 2))
     cv2.polylines(frame, [pts], True, (255, 0, 255), 5)
     pts2 = barcode.rect
     cv2.putText(frame, data, (pts2[0], pts2[1]), font, 0.9, (255, 0, 255), 2)
-    #return data == 'scanned qr'
-    
+    # return data == 'scanned qr'
+
+
 while True:
     success, frame = cap.read()
-    
+
     for barcode in decode(frame):
 
         pool = multiprocessing.Pool()
-        result_read = pool.map(read_qr, [barcode])           
+        result_read = pool.map(read_qr, [barcode])
         # return data == 'scanned qr' -> unlock
-        result_lock = pool.map(unlock,[3])
-        
-        
-    cv2.imshow('QR_SCAN', frame)
-    
+        result_lock = pool.map(unlock, [3])
+
+    cv2.imshow('frame', frame)
+
     if cv2.waitKey(1) & 0xFF == 27:
         break
-
-
-
-
