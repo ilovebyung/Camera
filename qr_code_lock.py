b116e5494fd2119feaf01f1205ccc72aef774a2c
ilovebyung <45321943+ilovebyung@users.noreply.github.com>
2020-12-04 11:47:47 +0900

    Update qr_code_lock.py

--- qr_code_lock.py
+++ qr_code_lock.py
@@ -10,8 +10,8 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 cap = cv2.VideoCapture(0)
 
 # full screen window
-cv2.namedWindow("frame", cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+#cv2.namedWindow("frame", cv2.WND_PROP_FULLSCREEN)
+#cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 def unlock(seconds):
     # GPIO setup
@@ -26,29 +26,32 @@ def unlock(seconds):
     GPIO.output(pin, GPIO.LOW)
     GPIO.cleanup()
 
-
-def read_qr(barcode):
-    data = barcode.data.decode('utf-8')
-    print(data)
-    pts = np.array([barcode.polygon], np.int32)
-    pts = pts.reshape((-1, 1, 2))
-    cv2.polylines(frame, [pts], True, (255, 0, 255), 5)
-    pts2 = barcode.rect
-    cv2.putText(frame, data, (pts2[0], pts2[1]), font, 0.9, (255, 0, 255), 2)
-    # return data == 'scanned qr'
-
-
 while True:
-    success, frame = cap.read()
 
+    success, frame = cap.read()
+    # resize frame
+    #frame = imutils.resize(frame, width=480)
+
+    Spool = multiprocessing.Pool()
+        
     for barcode in decode(frame):
 
-        pool = multiprocessing.Pool()
-        result_read = pool.map(read_qr, [barcode])
-        # return data == 'scanned qr' -> unlock
-        result_lock = pool.map(unlock, [3])
+        data = barcode.data.decode('utf-8')
+        # print(data)  # if data = 'data'
+        pts = np.array([barcode.polygon], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(frame, [pts], True, (255, 0, 255), 5)
+        pts2 = barcode.rect
+        cv2.putText(
+            frame, data, (pts2[0], pts2[1]), font, 0.9, (255, 0, 255), 2)
 
+        #if data == 'match':
+
+        result = pool.map(unlock, [3])
+        
+        #unlock(3)
+        
+        
     cv2.imshow('frame', frame)
-
     if cv2.waitKey(1) & 0xFF == 27:
         break
